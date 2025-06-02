aeb2df8498bc681289daf182ddfcc5cc68e7aabf
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-06-02 07:28:28 -0500

    Agregamos las 3 paginas que necesitamos

---
+++ PilamungaSEvaluacion2P/Views/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PilamungaSEvaluacion2P.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PilamungaSEvaluacion2P/Views/ChistesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PilamungaSEvaluacion2P.Views;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+	}
+}
