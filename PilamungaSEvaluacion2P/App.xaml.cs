f85c63655f6cdc800cfe99e64bcd905f94c917ec
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-06-02 07:21:34 -0500

    Agregar archivos de proyecto.

---
+++ PilamungaSEvaluacion2P/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PilamungaSEvaluacion2P
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
